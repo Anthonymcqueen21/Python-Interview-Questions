45349ce443fa85c32e1acd1c1f5bb49a68000ef9
Anthony Perez <stockcart21@yahoo.com>
2018-10-27 13:46:59 -0700

    Update Bubble_Sort.py

--- Algorithmic Questions/Bubble_Sort.py
+++ Algorithmic Questions/Bubble_Sort.py
@@ -1,4 +1,4 @@
 # A naive sorting that compares and swaps adjacent elements
 # Time complexity: 0(n**2)
 # Space Complexity: 0(1) Auxilary
-
+# A sequence of numbers that are sorted in lowest to highest order.
