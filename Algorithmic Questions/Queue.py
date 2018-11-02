be23777b55458c02be2e1ab5c96526b7d098c36f
Anthony Perez <stockcart21@yahoo.com>
2018-11-02 15:17:44 -0700

    Create Queue.py

---
+++ Algorithmic Questions/Queue.py
@@ -0,0 +1,13 @@
+# Queue is a linear data structure with a sequential collection process.
+
+
+
+
+
+
+# Python implementation
+from collections import deque
+
+class Queue:
+    def __init__(self):
+    
