014af3f85f9ddbf8e305e8949a798492709cab73
Anthony Perez <stockcart21@yahoo.com>
2018-12-19 16:35:39 -0800

    Update Bubble_Sort.py

--- Algorithmic Questions/Bubble_Sort.py
+++ Algorithmic Questions/Bubble_Sort.py
@@ -2,10 +2,17 @@
 # Time complexity: 0(n**2)
 # Space Complexity: 0(1) Auxilary
 # A sequence of numbers that are sorted in lowest to highest order.
+# Warning this is one of the slowest algorithms to use.
  
   
 Algorithmic Questions/Bubble_Sort.py
   
-class Solution:
- def bubble_sort:
-  print(sorting_solution)
+def sort(seq):
+ 
+ L = len(seq)
+ for i in range(L):
+    for n in range(1, L - i):
+        if seq[n] < seq[n - 1]:
+            seq[n - 1], seq[n] = seq[n], seq[n - 1]
+       
+ return seq
