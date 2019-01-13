79cae90532685d73cf160bb93e18c6d533e6b8e8
Anthony Perez <stockcart21@yahoo.com>
2019-01-13 08:37:57 -0800

    Update Queue.py

--- Algorithmic Questions/Queue.py
+++ Algorithmic Questions/Queue.py
@@ -5,4 +5,29 @@ from collections import deque
 
 class Queue:
     def __init__(self):
+        self._queue = deque([])
+        
+    def add(self.name):
+        """
+        Add element as the last item in Queue
+        """
+        self._queue.append(value)
+        
+    def remove(self):
+        """
+        Remove elements from the front
+        """
+        return self._queue.popleft()
+    
+    def is_empty(self):
+        """
+        Returns a boolean indication
+        """
+        return not len(self._queue)
+    
+    def size(self):
+        """
+        Return size of the Queue
+        """
+        return len(self._queue)
     
