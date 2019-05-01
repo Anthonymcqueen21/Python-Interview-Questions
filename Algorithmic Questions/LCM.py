bb8a31c85dc3b6720fd054cccaf4547151134a08
Anthony Perez <stockcart21@yahoo.com>
2019-05-01 15:01:08 -0700

    Update LCM.py

--- Algorithmic Questions/LCM.py
+++ Algorithmic Questions/LCM.py
@@ -5,15 +5,14 @@
    ----------------------
    Simple Implementation of the lowest common multilpe algorithm
 """
-
-# LCM Implementation <=
-def lcm(a,b):
    """
    Simple version of lcm code, that does not have any dependencies
    : param a: integer
    : param b: integer
    : rtype: The lowest common multiple of integers a and b
    """
+# LCM Implementation
+def lcm(a,b):
    tmp_ a = a
    while (tmp_a % b) != 0:
         tmp_a += b
