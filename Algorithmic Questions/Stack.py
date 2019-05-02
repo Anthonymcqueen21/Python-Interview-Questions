28752980e2f178a01ada79f65d0e2567e069c0a9
Anthony Perez <stockcart21@yahoo.com>
2019-05-02 07:29:29 -0700

    Update Stack.py

--- Algorithmic Questions/Stack.py
+++ Algorithmic Questions/Stack.py
@@ -1,4 +1,3 @@
-# Definition tech stack
 """
    A stack is using (Last in, First out) is an abstract data type that serves
    as a Collection of elements. Within two prime operations: Push, which adds element to the collection, and pop
@@ -22,10 +21,7 @@ class Stack:
        """
     
     def is_empty(self):
-      """
-      1 value is returned on empty 0 value returned
-      """
-      
+     
     def size(self):
       """
       Return size of stack
